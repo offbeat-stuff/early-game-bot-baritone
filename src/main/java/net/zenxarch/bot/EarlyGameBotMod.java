faba3dae798520c6c1639c553d4c0a1b0a813586
offbeat-stuff <sbteraparsecs@gmail.com>
2022-02-13 09:36:29 +0000

    added baritone dep

--- src/main/java/net/zenxarch/bot/EarlyGameBotMod.java
+++ src/main/java/net/zenxarch/bot/EarlyGameBotMod.java
@@ -1,21 +1,29 @@
 package net.zenxarch.bot;
 
-import net.fabricmc.api.ClientModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class EarlyGameMod implements ClientModInitializer {
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import baritone.api.BaritoneAPI;
+
+@Environment(EnvType.CLIENT)
+public class EarlyGameBotMod implements ClientModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+	public static final Logger LOGGER = LoggerFactory.getLogger("zenbot");
 
 	@Override
-	public void onInitialize() {
+	public void onInitializeClient() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
+		LOGGER.info("ZenBot loaded have fun botting.");
+	}
 
-		LOGGER.info("Hello Fabric world!");
+	public static Logger getLogger() {
+		return LOGGER;
 	}
 }
