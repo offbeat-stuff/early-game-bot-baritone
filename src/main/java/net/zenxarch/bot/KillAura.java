74e615893006abfc0579155a9e3faad8e85041de
sbgigapsec <sbgigaparsecs@gmail.com>
2022-03-03 09:47:10 +0000

    KillAura fix

--- src/main/java/net/zenxarch/bot/KillAura.java
+++ src/main/java/net/zenxarch/bot/KillAura.java
@@ -1,11 +1,22 @@
 package net.zenxarch.bot;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.mob.CreeperEntity;
+import net.minecraft.entity.mob.EndermanEntity;
+import net.minecraft.entity.mob.PiglinEntity;
+import net.minecraft.entity.passive.AnimalEntity;
+import net.minecraft.entity.passive.GolemEntity;
 import net.minecraft.entity.passive.PassiveEntity;
+import net.minecraft.entity.passive.TameableEntity;
+import net.minecraft.entity.passive.VillagerEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
 import net.minecraft.util.Hand;
@@ -21,6 +32,7 @@ public final class KillAura {
   public KillAura() {
     INSTANCE = this;
     this.targets = new ArrayList<LivingEntity>(0);
+    mc = MinecraftClient.getInstance();
   }
 
   public Double opD(LivingEntity e) {
@@ -31,26 +43,35 @@ public final class KillAura {
   }
 
   private boolean isGood(LivingEntity e) {
+    if(!e.isAlive() || e.isDead())
+      return false;
+    if(e == p)
+      return false;
     if (opD(e) > 25)
       return false;
-    if (e instanceof PassiveEntity)
+    if(e instanceof AnimalEntity && e.isBaby())
       return false;
-    if (e.isBlocking() || e.isInvulnerable())
+    if(e instanceof EndermanEntity eman && !eman.isAngryAt(mc.player))
       return false;
-    // if(e.hurtTime == 0) return true;
-    // if(e.isAttackable()) return true;
+    if(e instanceof PiglinEntity pig && !pig.isAngryAt(mc.player))
+      return false;
+    if(e instanceof GolemEntity || e instanceof VillagerEntity || e instanceof TameableEntity)
+      return false;
+    if(e instanceof PlayerEntity p && p.isCreative())
+      return false;
     return true;
   }
 
   private void updateTargets() {
     targets = new ArrayList<LivingEntity>(0);
     mc.world.getEntities().forEach(e -> {
-      if (!(e instanceof LivingEntity) || e == null)
+      if (e == null || !(e instanceof LivingEntity))
         return;
       if (isGood((LivingEntity)e)) {
         targets.add((LivingEntity)e);
       }
     });
+    targets.sort((e1, e2) -> sort(e1, e2));
   }
 
   private void lookAt(double x, double y, double z) {
@@ -74,12 +95,10 @@ public final class KillAura {
   }
 
   public boolean needsControl() {
-    updateTargets();
     return targets.size() > 0;
   }
 
   public void onTick() {
-    mc = MinecraftClient.getInstance();
     p = mc.player;
     if (p.isDead())
       return;
@@ -88,23 +107,41 @@ public final class KillAura {
     if (mc.currentScreen != null) {
       p.closeScreen();
     }
-    targets.sort((e1, e2) -> sort(e1, e2));
+    updateTargets();
     var e = targets.get(0);
     if (lookingAt() == e) {
-      if (e instanceof CreeperEntity &&
-          mc.player.getOffHandStack().getItem().equals(
-              Items.SHIELD)) {
-        if (opD(e) < 4) {
-          p.swingHand(Hand.OFF_HAND);
-        } else {
-          p.swingHand(Hand.MAIN_HAND);
-        }
-      } else {
-        p.swingHand(Hand.MAIN_HAND);
-      }
+      handleTarget(e);
     } else {
       lookAt(targets.get(0));
       syncRotation();
+    }
+  }
+
+  private void handleTarget(LivingEntity e){
+    switchItem();
+    if(e instanceof CreeperEntity && mc.player.getOffHandStack().getItem().equals(Items.SHIELD)){
+      mc.interactionManager.interactItem(mc.player, mc.world , Hand.OFF_HAND);
+      mc.player.swingHand(Hand.OFF_HAND);
+      return;
+    }
+    mc.interactionManager.attackEntity(mc.player, e);
+    mc.player.swingHand(Hand.OFF_HAND);
+  }
+
+  private void switchItem(){
+    var items = new Item[]{
+      Items.NETHERITE_AXE,Items.NETHERITE_SWORD,
+      Items.DIAMOND_AXE,Items.DIAMOND_SWORD,
+      Items.IRON_AXE,Items.IRON_SWORD,
+      Items.STONE_AXE,Items.STONE_SWORD,
+      Items.WOODEN_AXE,Items.WOODEN_SWORD
+    };
+    for(int i = 0;i < items.length;i++){
+      for(int j = 0;j < 8;j++){
+        if(p.getInventory().getStack(j).getItem() == items[i]){
+          p.getInventory().selectedSlot = j;
+        }
+      }
     }
   }
 
