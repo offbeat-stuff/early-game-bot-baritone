bc3cae7b43112e1cd6fdbffff00735d497a1143a
offbeat-stuff <sbteraparsecs@gmail.com>
2022-04-21 09:49:20 +0000

    More sensitive player arrow collision

--- src/main/java/net/zenxarch/bot/util/ProjectileEntitySimulator.java
+++ src/main/java/net/zenxarch/bot/util/ProjectileEntitySimulator.java
@@ -3,7 +3,6 @@ package net.zenxarch.bot.util;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.projectile.ArrowEntity;
 import net.minecraft.entity.projectile.ProjectileUtil;
-import net.minecraft.util.hit.EntityHitResult;
 import net.minecraft.util.hit.HitResult;
 import net.minecraft.util.math.Box;
 import net.minecraft.util.math.Vec3d;
@@ -22,18 +21,22 @@ public class ProjectileEntitySimulator {
       var hit = tickPos(arrow, p, vel);
       if (hit.getType() != HitResult.Type.MISS)
         nextPos = hit.getPos();
-      if (hit.getType() == HitResult.Type.ENTITY) {
-        var e = ((EntityHitResult)hit).getEntity();
-        if (e.equals(mc.player)) {
-          return i;
-        }
-      }
+      if (checkPlayerCollision(p, nextPos))
+        return i;
       if (nextPos.squaredDistanceTo(mc.player.getPos()) >= 16 * 16)
         return ticks;
       p = nextPos;
       vel = vel.multiply(airDrag).subtract(0, gravity, 0);
     }
     return ticks;
+  }
+
+  private static boolean checkPlayerCollision(Vec3d a, Vec3d b) {
+    var abox = new Box(a, b);
+    abox = abox.expand(0.3);
+    var pbox =
+        mc.player.getBoundingBox().stretch(mc.player.getVelocity()).expand(0.3);
+    return abox.intersects(pbox);
   }
 
   private static HitResult tickPos(ArrowEntity arrow, Vec3d pos, Vec3d vel) {
@@ -48,7 +51,7 @@ public class ProjectileEntitySimulator {
     }
 
     var hitResult2 = ProjectileUtil.getEntityCollision(
-        mc.world, arrow, p, np, new Box(p, np),
+        mc.world, mc.player, p, np, new Box(p, np),
         e -> { return e.isAlive() && !(e.isSpectator()) && e.collides(); });
     if (hitResult2 != null)
       return hitResult2;
