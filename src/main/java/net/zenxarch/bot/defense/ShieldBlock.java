53e500f7167819623f9448a6e2f857ce1cce5047
offbeat-stuff <sbteraparsecs@gmail.com>
2022-04-30 05:21:50 +0000

    basic shield check

--- src/main/java/net/zenxarch/bot/defense/ShieldBlock.java
+++ src/main/java/net/zenxarch/bot/defense/ShieldBlock.java
@@ -5,7 +5,9 @@ import static net.zenxarch.bot.ZenBot.mc;
 import net.minecraft.client.network.AbstractClientPlayerEntity;
 import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.entity.projectile.ProjectileEntity;
+import net.minecraft.item.BlockItem;
 import net.minecraft.item.Items;
+import net.minecraft.util.hit.HitResult;
 import net.zenxarch.bot.util.ClientPlayerHelper;
 
 public class ShieldBlock extends EntityDefenseModule {
@@ -38,14 +40,24 @@ public class ShieldBlock extends EntityDefenseModule {
   }
 
   private boolean tryBlock() {
-    if (mc.player.getOffHandStack().getItem().equals(Items.SHIELD) &&
-        DefenseStateManager.canPerformAction()) {
+    if (shieldCheck() && DefenseStateManager.canPerformAction()) {
       mc.options.useKey.setPressed(true);
       wasBlocking = true;
       return true;
     }
     unblockShield();
     return false;
+  }
+
+  private boolean shieldCheck() {
+    if (mc.player.getOffHandStack().getItem() != Items.SHIELD)
+      return false;
+    if (mc.player.getMainHandStack().isEmpty())
+      return true;
+    var item = mc.player.getMainHandStack().getItem();
+    if (item instanceof BlockItem)
+      return mc.crosshairTarget.getType() != HitResult.Type.BLOCK;
+    return !(item.isFood());
   }
 
   private void unblockShield() {
