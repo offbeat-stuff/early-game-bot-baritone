64b78765ca2a3ba392e271da5bfcbc33f51ef892
sbcloudshell <sbgigaparsecs@gmail.com>
2022-05-05 06:09:22 +0000

    Build fix

--- src/main/java/net/zenxarch/bot/mixin/PlayerEntityAccessor.java
+++ src/main/java/net/zenxarch/bot/mixin/PlayerEntityAccessor.java
@@ -1,10 +1,10 @@
 package net.zenxarch.bot.mixin;
 
-import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.LivingEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
-@Mixin(PlayerEntity.class)
+@Mixin(LivingEntity.class)
 public interface PlayerEntityAccessor {
-  @Accessor("lastAttackedTicks") public int getLastAttackedTicks();
+  @Accessor int getLastAttackedTicks();
 }
