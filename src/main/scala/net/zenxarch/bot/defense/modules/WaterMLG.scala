0fce0a54cfbcd6987c813f4e66d4d69d21fcf407
offbeat-stuff <sbteraparsecs@gmail.com>
2022-05-25 07:57:29 +0000

    well right click

--- src/main/scala/net/zenxarch/bot/defense/modules/WaterMLG.scala
+++ src/main/scala/net/zenxarch/bot/defense/modules/WaterMLG.scala
@@ -1,21 +1,24 @@
 package net.zenxarch.bot.defense.modules
 
-import net.zenxarch.bot.util.ClientPlayerHelper._
-
 import net.minecraft.block.Blocks
+import net.minecraft.block.FluidDrainable
+import net.minecraft.block.FluidFillable
+import net.minecraft.client.option.KeyBinding
+import net.minecraft.client.util.InputUtil
+import net.minecraft.item.BlockItem
+import net.minecraft.item.ItemStack
 import net.minecraft.item.Items
 import net.minecraft.util.Hand
+import net.minecraft.util.hit.BlockHitResult
+import net.minecraft.util.hit.HitResult
 import net.minecraft.util.math.BlockPos
+import net.minecraft.util.math.Direction
 import net.minecraft.world.RaycastContext.FluidHandling
 import net.minecraft.world.World
 import net.zenxarch.bot.defense.DefenseStateManager
 import net.zenxarch.bot.util.BlockPlacementUtils._
-import net.minecraft.item.ItemStack
-import net.minecraft.item.BlockItem
-import net.minecraft.util.math.Direction
 import net.zenxarch.bot.util.ClientPlayerHelper
-import net.minecraft.util.hit.HitResult
-import net.minecraft.util.hit.BlockHitResult
+import net.zenxarch.bot.util.ClientPlayerHelper._
 
 class WaterMLG extends Module("WaterMlg"):
   import Module.mc
@@ -49,12 +52,13 @@ class WaterMLG extends Module("WaterMlg"):
       pickItemSlot(saveItemSlot)
       ClientPlayerHelper.lookAt(vec.x, vec.y, vec.z)
       ClientPlayerHelper.syncRotation()
-      return place(bhit, Hand.MAIN_HAND)
+      KeyBinding.onKeyPressed(InputUtil.fromTranslationKey("key.use"))
+      true
     })
 
   private def checkSide(pos: BlockPos, side: Direction): Boolean =
     val block = mc.world.getBlockState(pos.offset(side))
-    side != Direction.UP && block.isSolidBlock(mc.world, pos)
+    side != Direction.UP && !block.getBlock().isInstanceOf[FluidFillable] && !block.getBlock().isInstanceOf[FluidDrainable] && block.isSideSolidFullSquare(mc.world, pos.offset(side),side.getOpposite())
 
   private def getLandingBlock(upto: Int): BlockPos =
     val start = mc.player.getBlockPos()
